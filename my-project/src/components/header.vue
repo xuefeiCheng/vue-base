2004aa8420b516748e059fa37f072d96ed8fe756
程雪飞 <1805711033@qq.com>
2018-06-03 18:09:13 +0800

    vue生命周期

--- my-project/src/components/header.vue
+++ my-project/src/components/header.vue
@@ -15,13 +15,44 @@ export default {
       Top:"给父组件传递的值"
     }
   },
-  methods:{
-  changeT:function(){
-  this.msg1 ='变更传递的值'
+  //生命周期
+  beforeCreate:function(){
+  //可加载动画
+  alert("组件实例化之前执行的函数");
   },
-  pushToP:function(){
-    this.$emit('parent',this.Top);
-  }
+  created:function(){
+  //dom还未建造，可以预先加载数据
+  //结束loading
+  alert("组件实例化完毕，但是页面还未显示");
+  },
+  beforeMount:function(){
+  //开始编译，挂载之前，创建虚拟dom
+  alert("挂载实例之前，页面仍未展示，虚拟dom已经配置，$el");
+  },
+  mounted:function(){
+  //假设数据需要dom创建完成后，在加载，可以在这里请求数据
+  alert("组件挂载后，此方法执行后，页面显示");
+
+  },
+  beforeUpdate:function(){
+  alert("组件更新前，页面仍未更新，虚拟dom配置");
+  },
+  updated:function(){
+  alert("组件更新完毕，页面展示");
+  },
+  beforeDestroy:function(){
+  alert("组件销毁前");
+  },
+  destroyed:function(){
+  alert("组件销毁完毕");
+  },
+  methods:{
+    changeT:function(){
+      this.msg1 ='变更传递的值'
+    },
+    pushToP:function(){
+      this.$emit('parent',this.Top);
+    }
   }
 }
 </script>
