d5448c6420c07bab843587cc636a4991057c256d
程雪飞 <1805711033@qq.com>
2018-06-03 16:42:37 +0800

    使用脚手架搭建vue项目，创建一个自定义组件haha

---
+++ my-project/src/App.vue
@@ -0,0 +1,32 @@
+<!-- 说明：初始项目，新创建组件haha -->
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <HAHA/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import HAHA from './components/haha'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,HAHA
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
