59bc96a3937346c7a203ac1c64fcd15b1b358bf7
程雪飞 <1805711033@qq.com>
2018-06-03 16:31:22 +0800

    round13

---
+++ Round13/app.js
@@ -0,0 +1,23 @@
+//实例化vue对象
+new Vue({
+	el:"#vue-app",
+	data:{
+		health:100,
+		ended:false
+	},
+	methods:{
+		push:function(){
+			this.health -= 10;
+			if(this.health<=0){
+				this.ended =true;
+			}
+		},
+		restart:function(){
+			this.health=100;
+			this.ended=false;
+		}
+	},
+	computed:{
+
+	}
+})
