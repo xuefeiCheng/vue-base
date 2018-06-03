56445a4b36ba99c3a5aad523111ca29f652c7187
程雪飞 <1805711033@qq.com>
2018-06-03 16:30:31 +0800

    round12

--- Round11/app.js
+++
@@ -1,8 +0,0 @@
-//实例化vue对象
-new Vue({
-	el:"#vue-app",
-	data:{
-		error:false,
-		success:false
-	}
-})
---
+++ Round12/app.js
@@ -0,0 +1,10 @@
+// 实例化vue对象
+new Vue({
+	el:"#vue-app",
+	data:{
+		users:[
+			{name:"xuefeiCheng",age:"20"},
+			{name:"fenfenQin",age:"20"}
+		]
+	}
+})
