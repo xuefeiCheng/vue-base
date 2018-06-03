3d7b376b709a4db65967921d1072eafe9aba604c
程雪飞 <1805711033@qq.com>
2018-06-03 18:58:15 +0800

    跨域 fetch

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -13,6 +13,23 @@ export default {
   name: 'App',
   data(){
   return {}
+  },
+  created:function(){
+    fetch("/api/test/testToken.php",{
+       method:"post",
+       headers:{
+         "Content-type":"application/json",
+         token:"f4c902c9ae5a2a9d8f84868ad064e706"
+       },
+       body:JSON.stringify({username:"henry",password:"321321"})
+     })
+     .then(result => {
+       // console.log(result)
+       return result.json()
+     })
+     .then(data => {
+       console.log(data)
+     })
   }
 }
 </script>
